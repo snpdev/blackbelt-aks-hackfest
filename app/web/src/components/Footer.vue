6ad525efe973ca189fd6e9614882c7ac8b25e67c
Mike Brooks <mike@snp.com>
2018-03-01 23:14:08 -0500

    Test the CI/CD Pipeline

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt Team
+        Skunks at Dawn
       </div>
       <div class="col-lg-6">
       </div>
